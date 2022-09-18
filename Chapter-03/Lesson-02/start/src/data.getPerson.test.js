e3f19006372a8ca99c356447861c213d7f0c3179
Ricky Garcia <rg3646@outlook.com>
2022-09-18 10:32:38 -0600

    completed Ch3 lesson 4

--- Chapter-03/Lesson-02/start/src/data.getPerson.test.js
+++ Chapter-03/Lesson-02/start/src/data.getPerson.test.js
@@ -9,3 +9,11 @@ test("Should retur null when not found", async () => {
   const person = await getPerson(-99);
   expect(person).toBeNull();
 })
+
+test("Should return correct person oject when found", async () => {
+  const person = await getPerson(1);
+  expect(person).toMatchObject({
+    firstName: "Bill",
+    lastName: "Peters",
+  })
+})
