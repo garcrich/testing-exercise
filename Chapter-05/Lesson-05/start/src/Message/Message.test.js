bdc05914b079a9d492239f01db3e38e415f2ddeb
Ricky Garcia <rg3646@outlook.com>
2022-09-22 19:10:58 -0600

    chap 05 lesson 05

--- Chapter-05/Lesson-05/start/src/Message/Message.test.js
+++ Chapter-05/Lesson-05/start/src/Message/Message.test.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { Message } from "./Message";
+import { logRoles } from '@testing-library/dom'
 
 test("Should render correct image when type is error", () => {
   render(<Message message="test" type="error" />);
+  expect(screen.getByRole("img", { name: "cross" })).toBeInTheDocument();
 });
