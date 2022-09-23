ccc42e3984f21844b5727b0342ed39a0c3ae94f3
Ricky Garcia <rg3646@outlook.com>
2022-09-23 17:39:07 -0600

    chap 06 lesson 4

--- Chapter-06/Lesson-01/start/src/ContactForm/ContactForm.test.js
+++ Chapter-06/Lesson-01/start/src/ContactForm/ContactForm.test.js
@@ -1,28 +1,27 @@
-import { render, screen} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { ContactForm } from "./ContactForm";
 import userEvent from "@testing-library/user-event";
-
-test("Clicking submit button without filling in form renders name validation error", async () => {
-  const user = userEvent.setup();
-  render(<ContactForm />);
-  screen.getByLabelText("Name").focus();
-  await user.keyboard("test");
-
-  screen.getByLabelText("Message").focus();
-  await user.keyboard("test");
-
-  await user.click(screen.getByText("Save"));
-  expect(await screen.findByText("Successfully saved")).toBeInTheDocument();
-});
-
-test("Pressing Enter when form is completed in should render a success message", async () => {
+test("Clicking Save button when form is completed in should render a success message", async () => {
   const user = userEvent.setup();
   render(<ContactForm />);
 
+  screen.getByLabelText("Name").focus();
+  await user.keyboard("test");
+
+  // await user.selectOptions(
+  //   screen.getByLabelText("Department"),
+  //   screen.getByText("Support")
+  // );
+
+  await user.selectOptions(screen.getByLabelText("Department"), [
+    screen.getByText("Support"),
+    screen.getByText("Finance"),
+  ]);
+
   screen.getByLabelText("Message").focus();
   await user.keyboard("test");
-  screen.getByLabelText("Name").focus();
-  await user.keyboard("test{enter}");
   
+  await user.click(screen.getByText("Save"));
+
   expect(await screen.findByText("Successfully saved")).toBeInTheDocument();
 });
